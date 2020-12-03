fbd1ab4c9f84af89a6f73934c94a53a8f138551f
project <my_projects_2020@outlook.com>
2020-12-03 09:59:34 +0200

    v1

---
+++ src/main.py
@@ -0,0 +1,92 @@
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn as uvicorn
+from server import *
+import webbrowser
+import time
+
+tags_metadata = [
+    {
+        "name": "Get Similar Words",
+        "description": "Returns all words in the dictionary that has the same permutation as the word.",
+        "externalDocs": {
+        "description":"example",
+            "url": "http://localhost:8000/api/v1/similar/?word=destain",
+        },
+    },
+    {
+        "name": "Get Statistics",
+        "description": "Return statistics about api",
+        "externalDocs": {
+            "description": "example",
+            "url": "http://localhost:8000/api/v1/stats/",
+        },
+    },
+    {
+            "name": "Docs Api",
+            "description": "Return all function in API",
+            "externalDocs": {
+                "description": "example",
+                "url": "http://localhost:8000/docs",
+            },
+        },
+]
+
+app = FastAPI( title="Api Similar Words",openapi_tags=tags_metadata)
+
+
+@app.get("/",tags=["Docs Api"])
+def docs_api():
+    webbrowser.open('http://localhost:8000/docs')
+
+
+
+
+@app.get("/api/v1/similar/",tags=["Get Similar Words"])
+async def get_words (word: Optional[str] = None):
+    start_time = time.time_ns()
+    if word:
+        response= Server().similar_words(word.lower(),start_time)
+        return response
+    else:
+        return {"word":[""]}
+
+
+
+@app.get("/api/v1/stats/",tags=["Get Statistics"])
+async def get_stat():
+        response = Server().metrics()
+        return response
+
+
+
+
+if __name__ == "__main__":
+    try:
+        #sort data for new dictionary
+        data = Data()
+        data.read_words()
+        data.save_sorted_words()
+        # run API server
+        uvicorn.run("main:app", host='localhost', port=8000, reload=True, workers=20)
+    except Exception as error:
+        print(error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/server.py
@@ -0,0 +1,89 @@
+import json
+import time
+
+class Data:
+    def __init__(self):
+        try:
+            self.txt = open('words_clean.txt', 'r')
+            self.file_prime = open('dict_prime.json', 'r')
+            self.dict_prime = json.load(self.file_prime)
+            self.file_stats = open('stats.json', 'r')
+            self.stats = json.load(self.file_stats)
+            self.sort_dict ={}
+            self.count_words = 0
+        except FileNotFoundError as error:
+            return error
+
+    #get word from file.
+    def read_words(self):
+        for word in self.txt:
+            self.count_words += 1
+            self.word_to_json(word.rstrip())
+        self.stats["totalWords"] = self.count_words # total word in DB for stats.
+        with open('stats.json', 'w')as json_file:
+            json.dump(self.stats, json_file, indent=4)
+    #calculate the sum of each word and write to the dictionary
+    def word_to_json(self,word):
+        sum_l = 1
+        try:
+            for l in word:
+                sum_l *= self.dict_prime[l]
+        except KeyError:
+            return "Please enter a valid word"
+
+        if sum_l  not in self.sort_dict:
+            self.sort_dict[sum_l] = [word]
+        else:
+            self.sort_dict[sum_l] += [word]
+
+    #save new dictionary in JSON file.
+    def save_sorted_words(self):
+        with open('dict_new.json', 'w')as json_file:
+            json.dump(self.sort_dict, json_file, indent=4)
+
+
+class Server:
+
+    def __init__(self):
+        try:
+            self.data_file = open('dict_new.json', 'r')
+            self.db_words = json.load(self.data_file)
+            self.file_prime = open('dict_prime.json', 'r')
+            self.dict_prime = json.load(self.file_prime)
+            self.file_stats = open('stats.json', 'r')
+            self.stats = json.load(self.file_stats)
+            self.sum_letter = 1
+            self.totalRequests = 0
+        except FileNotFoundError as error:
+            return error
+
+    # check if the word sum exists in the dictionary
+    def similar_words(self,word,start_time):
+        self.totalRequests += 1 # total Requests for stats.
+        self.stats["totalRequests"] += self.totalRequests
+        try:
+            for letter in word:
+                self.sum_letter *= self.dict_prime[letter]
+        except KeyError:
+            return "Please enter a valid word"
+        end_time = (time.time_ns() - start_time)
+        self.stats["avgProcessingTimeNs"] += [end_time] # avg run time for stats.
+        with open('stats.json', 'w')as json_file:
+            json.dump(self.stats, json_file, indent=4)
+        try:
+            # response to client.
+            self.db_words[str(self.sum_letter)].remove(word)
+            return {"similar": self.db_words[str(self.sum_letter)]}
+        except KeyError:
+            return {"similar": []}
+
+    # get statistic
+    def metrics(self):
+        self.stats["avgProcessingTimeNs"] = [sum(self.stats["avgProcessingTimeNs"])/ len(self.stats["avgProcessingTimeNs"])]
+
+        with open('stats.json', 'w')as json_file:
+            json.dump(self.stats, json_file, indent=4)
+
+        return self.stats
+
+
